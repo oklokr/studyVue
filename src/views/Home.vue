d04568fa70e9064828fd8d9634e9bfc49eee8f7b
aig321 <aig321.cafe24app.com>
2021-03-17 17:30:53 +0900

    start

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrap">
+    <p>test</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "test",
+}
+</script>
+
+<style lang="scss">
+  @import url("../assets/css/test.scss");
+</style>
